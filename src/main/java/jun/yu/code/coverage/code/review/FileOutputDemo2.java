dd5133a31956d12c2a542e7fa79daac6feaae272
jyu <jun.yu@am.sony.com>
2016-10-19 12:10:32 -0700

    add new file

---
+++ src/main/java/jun/yu/code/coverage/code/review/FileOutputDemo2.java
@@ -0,0 +1,32 @@
+package jun.yu.code.coverage.code.review;
+
+import java.io.*;
+
+class FileOutputDemo2 
+{   
+
+
+        public static void main(String[] args) throws IOException
+        {              
+                FileOutputStream out; // declare a file output object
+                PrintStream p; // declare a print stream object
+
+                try
+                {
+                        // Create a new file output stream
+                        // connected to "myfile.txt"
+                        out = new FileOutputStream("myfile.txt");
+
+                        // Connect print stream to the output stream
+                        p = new PrintStream( out );
+        
+                        p.println ("This is written to a file");
+
+                        p.close();
+                }
+                catch (Exception e)
+                {
+                    throw new IOException(e);
+                }
+        }
+}
