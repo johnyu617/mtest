eeaf61dcd25287e4e7c1de570d77241e58757b59
jyu <jun.yu@am.sony.com>
2016-10-19 11:43:11 -0700

    enable throw exception

--- src/main/java/coverage2/FileOutputDemo.java
+++ src/main/java/coverage2/FileOutputDemo.java
@@ -26,7 +26,7 @@ class FileOutputDemo
                 }
                 catch (Exception e)
                 {
-                    //throw new RuntimeException(e);
+                    throw new RuntimeException(e);
                 }
         }
 }
